03db80f4b78a71a9450081b59af709d387a2fdd2
BraveCaperCat2 <113925022+BraveCaperCat2@users.noreply.github.com>
2024-11-01 10:35:59 +0000

    Update settings.lua
    Add a setting to add module slots.

--- settings.lua
+++ settings.lua
@@ -1,1 +1,8 @@
-
+data:extend{
+    {   name = 'quality-rebalance_module-slots',
+        type = 'bool-setting',
+        setting_type = 'startup',
+        default_value = false,
+        order = 'a'
+    }
+}
